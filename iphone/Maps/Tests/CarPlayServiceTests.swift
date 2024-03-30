0fa4fa3ec70b46e6330c0171de5e4b8011c5302e
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2024-03-30 17:14:53 +0400

    [ios] CarPlay estimates test example
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

---
+++ iphone/Maps/Tests/CarPlayServiceTests.swift
@@ -0,0 +1,38 @@
+import XCTest
+@testable import Organic_Maps__Debug_
+
+final class CarPlayServiceTests: XCTestCase {
+
+  var carPlayService: CarPlayService!
+
+  override func setUp() {
+    carPlayService = CarPlayService()
+  }
+
+  override func tearDown() {
+    carPlayService = nil
+  }
+
+  func testCreateEstimates() {
+    let routeInfo = RouteInfo(timeToTarget: 100,
+                              targetDistance: 25.2,
+                              targetUnitsIndex: 1, // km
+                              distanceToTurn: 0.5,
+                              turnUnitsIndex: 0, // m
+                              streetName: "Niamiha",
+                              turnImageName: nil,
+                              nextTurnImageName: nil,
+                              speedMps: 40.5,
+                              speedLimitMps: 60,
+                              roundExitNumber: 0)
+    let estimates = carPlayService.createEstimates(routeInfo: routeInfo)
+
+    guard let estimates else {
+      XCTFail("Estimates should not be nil.")
+      return
+    }
+
+    XCTAssertEqual(estimates.distanceRemaining, Measurement<UnitLength>(value: 25.2, unit: .kilometers))
+    XCTAssertEqual(estimates.timeRemaining, 100)
+  }
+}
